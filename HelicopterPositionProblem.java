9bfd90a1d5a3f9a9a316f4cc43a02ca4aa78b175
yabeshchy1000 <yabeshchaudhary888@gmail.com>
2026-07-29 11:43:37 +0545

    Easy College Assignment

---
+++ HelicopterPositionProblem.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class HelicopterPositionProblem {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the circle : ");
+        int r = input.nextInt();
+        System.out.println("Enter the position of Helicopter : ");
+        System.out.print("Enter x-axis : ");
+        int x = input.nextInt();
+        System.out.print("Enter y-axis : ");
+        int y = input.nextInt();
+        
+        double TempRadius = Math.sqrt(x*x + y*y);
+        if (TempRadius == r) {
+            System.out.println("Helicopter is exactly on circumference");
+        } else if (TempRadius == 0) {
+            System.out.println("Helicopter is in the center of the circle");
+        } else if (TempRadius > r) {
+            System.out.println("Helicopter is outside the circle");
+        } else {
+            System.out.println("Helicopter is inside the circle");
+        }
+        input.close();
+    }
+}
